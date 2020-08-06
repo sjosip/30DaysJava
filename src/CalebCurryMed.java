b55cabb1af8fe642d1a60761e42d7ff46b061331
Josip Stander <stander.josip@gmail.com>
2020-08-06 20:35:35 +0200

    Iterators

--- src/CalebCurryMed.java
+++ src/CalebCurryMed.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class CalebCurryMed {
 
@@ -6,15 +7,34 @@ public class CalebCurryMed {
     public static void main(String[] args)  {
 
 
-        // Stack examples
+        // Iterator
         LinkedList<String> names = new LinkedList<String>();
         names.push("Caleb");
         names.push("Cobra");
-        names.push("Chuppacabra");
+        names.push("Doris");
 
-        System.out.println(names.pop());
-        System.out.println(names.removeFirst());
-        System.out.println(names.remove());
+        //Iterator <String> iterator = names.iterator();
+        ListIterator <String> iterator = names.listIterator();
+       /* System.out.println(iterator.next());// last
+        System.out.println(iterator.next());// second
+        System.out.println(iterator.next());//first*/
+
+       iterator.next();
+       iterator.next();
+       iterator.add("Jerry");
+
+        names.add(2, "Lenny");
+
+       /* while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }*/
+
+        //OR ova ili ona petlja rade isto
+
+        // for each element in the array
+        for (String s: names) {
+            System.out.println(s);
+        }
 
     }
 }
